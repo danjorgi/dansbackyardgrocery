bfb8daddb491f73b1b318a8878d02673c6103244
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2024-04-15 18:34:45 -0600

    Added controllers, wrote logic for UserController.

---
+++ groceryApp/src/main/java/com/store/groceryApp/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.store.groceryApp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.store.groceryApp.dtos.UserDto;
+import com.store.groceryApp.services.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public List<String> addUser(@RequestBody UserDto userDto) {
+        String passHash = passwordEncoder.encode(userDto.getUserPassword());
+        userDto.setUserPassword(passHash);
+        return userService.addUser(userDto);
+    }
+
+    @PostMapping("/login")
+    public List<String> userLogin(@RequestBody UserDto userDto) {
+        return userService.userLogin(userDto);
+    }
+}
